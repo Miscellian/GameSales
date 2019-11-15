46b743bda39cd5ea632a9bae309226cc637b1c76
120700max <120700max@gmail.com>
2019-11-15 23:01:27 +0200

    add find game price

--- GameSales-Steam/steam_api.py
+++ GameSales-Steam/steam_api.py
@@ -4,6 +4,7 @@ class SteamAPI:
 
     def __init__(self):
         self.api_url = "http://api.steampowered.com/"
+        self.store_url = "https://store.steampowered.com/"
 
     def __getGameIdByName(self, name):
         method = "ISteamApps/GetAppList/v0002"
@@ -12,15 +13,21 @@ class SteamAPI:
             if (name == game['name']):
                 return game['appid']
 
-    def getNewsForGame(self, name):
-        app_id = self.__getGameIdByName(name)
-        method = "ISteamNews/GetNewsForApp/v0002/"
-        json = requests.get(self.api_url + method + "?appid=%d&count=3&maxlength=300&format=json" % app_id).json()
-        news = []
-        for new in json['appnews']['newsitems']:
-            news.append(new['title'])
-        return news
-        
+    def __getAppDetailsForGame(self, id):
+        method = "api/appdetails?appids="
+        json = requests.get(self.store_url + method + str(id)).json()
+        return json
 
+    def getPriceOverviewForGame(self, name):
+        id = self.__getGameIdByName(name)
+        app_details = self.__getAppDetailsForGame(id)
+        price_overview = app_details['%d' % id]['data']['price_overview']
+        return price_overview
+
+        
 api = SteamAPI()
-print(api.getNewsForGame("Terraria"))
+price_overview = api.getPriceOverviewForGame("Street Fighter V")
+price_message = "Discount: " + str(price_overview['discount_percent']) + "%\n"
+price_message += "Price now: " + price_overview['final_formatted'] + "\n"
+price_message += "Price without discount: " + price_overview['initial_formatted']
+print(price_message)
